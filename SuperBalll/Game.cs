4720604f632f4e42ae646edf19456e783125d189
Влад <vladikgasya@gmail.com>
2014-12-19 13:09:39 +0200

    всё ещё написание движка

--- SuperBalll/Game.cs
+++ SuperBalll/Game.cs
@@ -9,10 +9,31 @@ namespace SuperBalll
 {
     class Game
     {
-        Bitmap Image;
-        Graphics G;
+
+        Bitmap image;
+        /// <summary>
+        /// Главный холст (картинка всего игрового поля)
+        /// </summary>
+        public Bitmap Image
+        {
+            get { return image; }
+            set { image = value; }
+        }
+        /// <summary>
+        /// Графика, на ней будут происходить все отрисовки
+        /// </summary>
+        private Graphics G;
         PictureBox GameWindow;
-        ObjectCollection Objects;
+
+        ObjectCollection objects;
+        /// <summary>
+        /// Список текущих игровых объектов
+        /// </summary>
+        internal ObjectCollection Objects
+        {
+            get { return objects; }
+            set { objects = value; }
+        }
 
         /// <summary>
         /// Создает экземпляр игры, и связывает его с игровым окном, которым является PictureBox
@@ -23,11 +44,17 @@ namespace SuperBalll
             this.GameWindow = gameWindow;
             this.Image = new Bitmap(GameWindow.Width, GameWindow.Height);
         }
-       
+        /// <summary>
+        /// Создает экземпляр игры, и связывает его с игровым окном, которым является PictureBox
+        /// </summary>
+        /// <param name="gameWindow">PictureBox - игровое окно</param>
         public void Draw()
         {
             G.Clear(Color.White);
-            foreach()
+            foreach(GameObject obj in Objects.Items)
+            {
+                obj.Draw(G);
+            }
             GameWindow.Image = Image;
         }
         
